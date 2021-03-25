bd336e9ef23d6a0c7c5268df04cbe66c6c56aaeb
Alex Renoki <rennokki@gmail.com>
2021-03-25 13:11:26 +0200

    Fixed exceptions throwing out

--- src/Kinds/K8sResource.php
+++ src/Kinds/K8sResource.php
@@ -10,6 +10,7 @@ use RenokiCo\PhpK8s\Contracts\Loggable;
 use RenokiCo\PhpK8s\Contracts\Scalable;
 use RenokiCo\PhpK8s\Contracts\Watchable;
 use RenokiCo\PhpK8s\Exceptions\KubernetesAPIException;
+use RenokiCo\PhpK8s\Exceptions\KubernetesLogsException;
 use RenokiCo\PhpK8s\Exceptions\KubernetesScalingException;
 use RenokiCo\PhpK8s\Exceptions\KubernetesWatchException;
 use RenokiCo\PhpK8s\KubernetesCluster;
