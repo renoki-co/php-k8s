c68ad33af0e4f2d8eb53d28a8412123c8756d128
Alex Renoki <rennokki@gmail.com>
2021-03-26 14:17:03 +0200

    Fixed PDB test

--- src/Kinds/K8sPodDisruptionBudget.php
+++ src/Kinds/K8sPodDisruptionBudget.php
@@ -4,11 +4,13 @@ namespace RenokiCo\PhpK8s\Kinds;
 
 use RenokiCo\PhpK8s\Contracts\InteractsWithK8sCluster;
 use RenokiCo\PhpK8s\Contracts\Watchable;
+use RenokiCo\PhpK8s\Traits\HasSelector;
 use RenokiCo\PhpK8s\Traits\HasSpec;
 use RenokiCo\PhpK8s\Traits\HasStatus;
 
 class K8sPodDisruptionBudget extends K8sResource implements InteractsWithK8sCluster, Watchable
 {
+    use HasSelector;
     use HasSpec;
     use HasStatus;
 
