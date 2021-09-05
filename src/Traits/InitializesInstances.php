c85f0febc116adcbe4886a148cf7bcfbbe2c7d91
Alex Renoki <rennokki@gmail.com>
2021-09-05 16:03:33 +0300

    Added instance & resource initialization in two traits

---
+++ src/Traits/InitializesInstances.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace RenokiCo\PhpK8s\Traits;
+
+use RenokiCo\PhpK8s\Instances\Rule;
+use RenokiCo\PhpK8s\Instances\Probe;
+use RenokiCo\PhpK8s\Instances\Volume;
+use RenokiCo\PhpK8s\Instances\Subject;
+use RenokiCo\PhpK8s\Instances\Affinity;
+use RenokiCo\PhpK8s\Instances\Container;
+use RenokiCo\PhpK8s\Instances\Expression;
+use RenokiCo\PhpK8s\Instances\ResourceMetric;
+use RenokiCo\PhpK8s\Instances\ResourceObject;
+
+trait InitializesInstances
+{
+    /**
+     * Create a new container instance.
+     *
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Instances\Container
+     */
+    public static function container(array $attributes = [])
+    {
+        return new Container($attributes);
+    }
+
+    /**
+     * Create a new probe instance.
+     *
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Instances\Probe
+     */
+    public static function probe(array $attributes = [])
+    {
+        return new Probe($attributes);
+    }
+
+    /**
+     * Create a new metric instance.
+     *
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Instances\ResourceMetric
+     */
+    public static function metric(array $attributes = [])
+    {
+        return new ResourceMetric($attributes);
+    }
+
+    /**
+     * Create a new object instance.
+     *
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Instances\ResourceObject
+     */
+    public static function object(array $attributes = [])
+    {
+        return new ResourceObject($attributes);
+    }
+
+    /**
+     * Create a new rule instance.
+     *
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Instances\Rule
+     */
+    public static function rule(array $attributes = [])
+    {
+        return new Rule($attributes);
+    }
+
+    /**
+     * Create a new subject instance.
+     *
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Instances\Subject
+     */
+    public static function subject(array $attributes = [])
+    {
+        return new Subject($attributes);
+    }
+
+    /**
+     * Create a new volume instance.
+     *
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Instances\Volume
+     */
+    public static function volume(array $attributes = [])
+    {
+        return new Volume($attributes);
+    }
+
+    /**
+     * Create a new affinity instance.
+     *
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Instances\Affinity
+     */
+    public static function affinity(array $attributes = [])
+    {
+        return new Affinity($attributes);
+    }
+
+    /**
+     * Create a new expression instance.
+     *
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Instances\Expression
+     */
+    public static function expression(array $attributes = [])
+    {
+        return new Expression($attributes);
+    }
+}
---
+++ src/Traits/InitializesResources.php
@@ -0,0 +1,306 @@
+<?php
+
+namespace RenokiCo\PhpK8s\Traits;
+
+use RenokiCo\PhpK8s\Kinds\K8sJob;
+use RenokiCo\PhpK8s\Kinds\K8sPod;
+use RenokiCo\PhpK8s\Kinds\K8sNode;
+use RenokiCo\PhpK8s\Kinds\K8sRole;
+use RenokiCo\PhpK8s\Kinds\K8sEvent;
+use RenokiCo\PhpK8s\Kinds\K8sSecret;
+use RenokiCo\PhpK8s\Kinds\K8sCronJob;
+use RenokiCo\PhpK8s\Kinds\K8sIngress;
+use RenokiCo\PhpK8s\Kinds\K8sService;
+use RenokiCo\PhpK8s\Kinds\K8sConfigMap;
+use RenokiCo\PhpK8s\Kinds\K8sDaemonSet;
+use RenokiCo\PhpK8s\Kinds\K8sNamespace;
+use RenokiCo\PhpK8s\Kinds\K8sDeployment;
+use RenokiCo\PhpK8s\Kinds\K8sClusterRole;
+use RenokiCo\PhpK8s\Kinds\K8sRoleBinding;
+use RenokiCo\PhpK8s\Kinds\K8sStatefulSet;
+use RenokiCo\PhpK8s\Kinds\K8sStorageClass;
+use RenokiCo\PhpK8s\Kinds\K8sServiceAccount;
+use RenokiCo\PhpK8s\Kinds\K8sPersistentVolume;
+use RenokiCo\PhpK8s\Kinds\K8sClusterRoleBinding;
+use RenokiCo\PhpK8s\Kinds\K8sPodDisruptionBudget;
+use RenokiCo\PhpK8s\Kinds\K8sPersistentVolumeClaim;
+use RenokiCo\PhpK8s\Kinds\K8sHorizontalPodAutoscaler;
+
+trait InitializesResources
+{
+    /**
+     * Create a new Node kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sNode
+     */
+    public static function node($cluster = null, array $attributes = [])
+    {
+        return new K8sNode($cluster, $attributes);
+    }
+
+    /**
+     * Create a new Event kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sEvent
+     */
+    public static function event($cluster = null, array $attributes = [])
+    {
+        return new K8sEvent($cluster, $attributes);
+    }
+
+    /**
+     * Create a new Namespace kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sNamespace
+     */
+    public static function namespace($cluster = null, array $attributes = [])
+    {
+        return new K8sNamespace($cluster, $attributes);
+    }
+
+    /**
+     * Create a new ConfigMap kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sConfigMap
+     */
+    public static function configmap($cluster = null, array $attributes = [])
+    {
+        return new K8sConfigMap($cluster, $attributes);
+    }
+
+    /**
+     * Create a new Secret kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sSecret
+     */
+    public static function secret($cluster = null, array $attributes = [])
+    {
+        return new K8sSecret($cluster, $attributes);
+    }
+
+    /**
+     * Create a new Ingress kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sIngress
+     */
+    public static function ingress($cluster = null, array $attributes = [])
+    {
+        return new K8sIngress($cluster, $attributes);
+    }
+
+    /**
+     * Create a new Service kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sService
+     */
+    public static function service($cluster = null, array $attributes = [])
+    {
+        return new K8sService($cluster, $attributes);
+    }
+
+    /**
+     * Create a new StorageClass kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sStorageClass
+     */
+    public static function storageClass($cluster = null, array $attributes = [])
+    {
+        return new K8sStorageClass($cluster, $attributes);
+    }
+
+    /**
+     * Create a new PersistentVolume kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sPersistentVolume
+     */
+    public static function persistentVolume($cluster = null, array $attributes = [])
+    {
+        return new K8sPersistentVolume($cluster, $attributes);
+    }
+
+    /**
+     * Create a new PersistentVolumeClaim kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sPersistentVolumeClaim
+     */
+    public static function persistentVolumeClaim($cluster = null, array $attributes = [])
+    {
+        return new K8sPersistentVolumeClaim($cluster, $attributes);
+    }
+
+    /**
+     * Create a new Pod kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sPod
+     */
+    public static function pod($cluster = null, array $attributes = [])
+    {
+        return new K8sPod($cluster, $attributes);
+    }
+
+    /**
+     * Create a new StatefulSet kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sStatefulSet
+     */
+    public static function statefulSet($cluster = null, array $attributes = [])
+    {
+        return new K8sStatefulSet($cluster, $attributes);
+    }
+
+    /**
+     * Create a new Deployment kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sDeployment
+     */
+    public static function deployment($cluster = null, array $attributes = [])
+    {
+        return new K8sDeployment($cluster, $attributes);
+    }
+
+    /**
+     * Create a new Job kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sJob
+     */
+    public static function job($cluster = null, array $attributes = [])
+    {
+        return new K8sJob($cluster, $attributes);
+    }
+
+    /**
+     * Create a new CronJob kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sCronJob
+     */
+    public static function cronjob($cluster = null, array $attributes = [])
+    {
+        return new K8sCronJob($cluster, $attributes);
+    }
+
+    /**
+     * Create a new DaemonSet kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sDaemonSet
+     */
+    public static function daemonSet($cluster = null, array $attributes = [])
+    {
+        return new K8sDaemonSet($cluster, $attributes);
+    }
+
+    /**
+     * Create a new HorizontalPodAutoscaler kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sHorizontalPodAutoscaler
+     */
+    public static function horizontalPodAutoscaler($cluster = null, array $attributes = [])
+    {
+        return new K8sHorizontalPodAutoscaler($cluster, $attributes);
+    }
+
+    /**
+     * Create a new ServiceAccount kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sServiceAccount
+     */
+    public static function serviceAccount($cluster = null, array $attributes = [])
+    {
+        return new K8sServiceAccount($cluster, $attributes);
+    }
+
+    /**
+     * Create a new Role kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sRole
+     */
+    public static function role($cluster = null, array $attributes = [])
+    {
+        return new K8sRole($cluster, $attributes);
+    }
+
+    /**
+     * Create a new ClusterRole kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sClusterRole
+     */
+    public static function clusterRole($cluster = null, array $attributes = [])
+    {
+        return new K8sClusterRole($cluster, $attributes);
+    }
+
+    /**
+     * Create a new RoleBinding kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sRoleBinding
+     */
+    public static function roleBinding($cluster = null, array $attributes = [])
+    {
+        return new K8sRoleBinding($cluster, $attributes);
+    }
+
+    /**
+     * Create a new ClusterRoleBinding kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sClusterRoleBinding
+     */
+    public static function clusterRoleBinding($cluster = null, array $attributes = [])
+    {
+        return new K8sClusterRoleBinding($cluster, $attributes);
+    }
+
+    /**
+     * Create a new PodDisruptionBudget kind.
+     *
+     * @param  \RenokiCo\PhpK8s\KubernetesCluster|null  $cluster
+     * @param  array  $attributes
+     * @return \RenokiCo\PhpK8s\Kinds\K8sPodDisruptionBudget
+     */
+    public static function podDisruptionBudget($cluster = null, array $attributes = [])
+    {
+        return new K8sPodDisruptionBudget($cluster, $attributes);
+    }
+}
