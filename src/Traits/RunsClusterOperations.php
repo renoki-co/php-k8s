b64416f86aa0a41a106b3a917f98c2da1bdc080f
Alex Renoki <rennokki@gmail.com>
2021-10-24 16:23:51 +0300

    Revert "Merge pull request #147 from renoki-co/fix/2.x-resourceversion-not-being-set-properly"
    This reverts commit d0851d1815191d9b4175e478f09ee383529fc53e, reversing
    changes made to c9f66aaca43addb24f416559711823107432ee91.

--- src/Traits/RunsClusterOperations.php
+++ src/Traits/RunsClusterOperations.php
@@ -96,21 +96,6 @@ trait RunsClusterOperations
     }
 
     /**
-     * Make sure to sync the resource version with the original.
-     *
-     * @return $this
-     */
-    public function refreshResourceVersion()
-    {
-        $this->setAttribute(
-            'metadata.resourceVersion',
-            $this->original['metadata']['resourceVersion']
-        );
-
-        return $this;
-    }
-
-    /**
      * Create or update the resource, wether the resource exists
      * or not within the cluster.
      *
@@ -234,7 +219,6 @@ trait RunsClusterOperations
     public function update(array $query = ['pretty' => 1]): bool
     {
         $this->refreshOriginal();
-        $this->refreshResourceVersion();
 
         // If it didn't change, no way to trigger the change.
         if (! $this->hasChanged()) {
