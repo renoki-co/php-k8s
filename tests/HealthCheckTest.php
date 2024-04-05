1f655fbf7651b2c5055c8aaabdbe63525c3d6a8f
Nyamort <67233336+Nyamort@users.noreply.github.com>
2024-04-05 21:50:32 +0000

    Add isReady and isLive methods to KubernetesCluster

---
+++ tests/HealthCheckTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use RenokiCo\PhpK8s\KubernetesCluster;
+use RenokiCo\PhpK8s\Test\TestCase;
+
+class HealthCheckTest extends TestCase
+{
+    /**
+     * @group health-check
+     *
+     */
+    public function testIsLive()
+    {
+        $this->assertTrue($this->cluster->isLive());
+    }
+
+    /**
+     * @group health-check
+     *
+     */
+    public function testIsReady()
+    {
+        $this->assertTrue($this->cluster->isReady());
+    }
+}
