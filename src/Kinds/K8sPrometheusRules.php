6f03629eb84bf802c8dab6fcd31c741d1b6dc301
dylan <dylan.caudoux@septeo.com>
2024-09-24 14:39:46 +0200

    add kind prometheus

---
+++ src/Kinds/K8sPrometheusRules.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace RenokiCo\PhpK8s\Kinds;
+
+use RenokiCo\PhpK8s\Contracts\InteractsWithK8sCluster;
+use RenokiCo\PhpK8s\Contracts\Watchable;
+use RenokiCo\PhpK8s\Traits\Resource\HasSpec;
+
+class K8sPrometheusRules extends K8sResource implements InteractsWithK8sCluster, Watchable{
+    /**
+     * The resource Kind parameter.
+     *
+     * @var null|string
+     */
+    protected static $kind = 'PrometheusRule';
+
+    /**
+     * The default version for the resource.
+     *
+     * @var string
+     */
+    protected static $defaultVersion = 'monitoring.coreos.com/v1';
+
+    /**
+     * Wether the resource has a namespace.
+     *
+     * @var bool
+     */
+    protected static $namespaceable = true;
+
+
+}
